2067ae1e50f8a463842fdfe9cb85ce316bc64e33
Philani Ndlovu <philani.software.dev@gmail.com>
2022-05-04 16:53:02 +0200

    front-end
    frontend template complete

--- spa_blog/resources/js/app.js
+++ spa_blog/resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+import router from './router';
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+window.Vue = require('vue').default;
+Vue.use(ViewUI);
+
+Vue.component('mainapp', require('./components/mainapp.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    router
+});
